3166c8219d976ff10f83aae669910b244b8f8fcb
Alec Chappell <32017824+alecclyde@users.noreply.github.com>
2024-08-22 09:44:54 -0400

    Update enum_auto_install.sh

--- enum_auto_install.sh
+++ enum_auto_install.sh
@@ -9,11 +9,10 @@ if [ "$(id -u)" -ne 0 ]; then
 fi
 
 # check and install necessary dependencies
-sudo apt install -y curl wget git make pip pipx
-
-# Update and install system dependencies
 sudo apt update
-sudo apt install -y nmap tshark dnsenum enum4linux screen
+
+sudo apt install -y curl wget git make pip pipx libssl-dev
+sudo apt install -y nmap tshark dnsenum enum4linux screen gowitness autorecon nuclei netexec
 
 # Install Python packages
 sudo pip install --upgrade pip
@@ -30,17 +29,6 @@ else
     echo "Google Chrome is already installed."
 fi
 
-# Install GoWitness
-if [ ! -f /opt/gowitness/gowitness-linux-amd64 ]; then
-    echo "Installing GoWitness..."
-    sudo mkdir -p /opt/gowitness/
-    wget https://github.com/sensepost/gowitness/releases/download/2.5.1/gowitness-2.5.1-linux-amd64 -O gowitness-linux-amd64
-    sudo mv gowitness-linux-amd64 /opt/gowitness/gowitness-linux-amd64
-    sudo chmod a+x /opt/gowitness/gowitness-linux-amd64
-else
-    echo "GoWitness is already installed."
-fi
-
 # Install rdpscan
 if [ -d /opt/rdpscan ]; then
     cd /opt/rdpscan
@@ -53,6 +41,5 @@ else
     make
 fi
 
-# Install crackmapexec and other tools using pipx
-pipx install git+https://github.com/Pennyw0rth/NetExec
+# Install crackmapexec
 pipx install git+https://github.com/byt3bl33d3r/CrackMapExec
